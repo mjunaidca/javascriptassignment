a17540a6f8de5ed2d49340e09800bb7dcd662b0a
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2022-10-22 23:28:04 +0500

    Javascript Assignment Last Question

--- question45/index.js
+++ question45/index.js
@@ -1,6 +1,10 @@
-function mycar(car){
-    car = {
-        manufacturer: 
-        model 
-
-}
+function mycar(manufacturer, model, ...arguments){
+    return{
+        manufacturer : manufacturer,
+        model : model,
+        arguments : arguments
+    }
+};
+console.log(mycar('Mustung', '2012'));
+console.log(mycar('Ferrari', '2017', 'green', '2 doors'));
+console.log(mycar('Ford', '1987', 'red', 'automatic', '4 doors'));
