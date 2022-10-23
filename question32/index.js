dcaf97bc0b7df5caa6467416838cad0e7b6715e6
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2022-10-23 10:29:08 +0500

    Update index.js

--- question32/index.js
+++ question32/index.js
@@ -1,12 +1,13 @@
 // Question 32
 
 let current_users = ['user1', 'user2', 'user3', 'user4', 'user5'];
-let new_users = ['user2', 'user4', 'user5', 'user8', 'user9'];
-
-for (let i = 0; i < new_users.length; i++) {
-    if (current_users.includes(new_users[i])) {
-        console.log(new_users[i] + ' is already taken');
+let new_users = ['User2', 'user4', 'user5', 'user8', 'user9'];
+let currentUsers = current_users.map((user) => user.toLowerCase());
+let newUsers = new_users.map((user) => user.toLowerCase());
+for (let i = 0; i < newUsers.length; i++) {
+    if (currentUsers.includes(newUsers[i])) {
+        console.log(newUsers[i] + ' is already taken');
     } else {
-        console.log(new_users[i] + ' is available');
+        console.log(newUsers[i] + ' is available');
     }
 }
